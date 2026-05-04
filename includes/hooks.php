7b70b63397cd52a8bea28d4038097633783e7504
GITFalz <bjornarvalkea@gmail.com>
2026-05-04 13:02:36 +0200

    added some more debug

--- includes/hooks.php
+++ includes/hooks.php
@@ -816,7 +816,10 @@ function ba_chatbot_upload_page_handler()
             wp_die();
         }
 
-        ai_chatbot_store_page(intval($page_id));
+        if (!ai_chatbot_store_page(intval($page_id))) {
+            wp_send_json_error(['message' => 'Failed to add page ' . $page_id . ", for reason: It already exists in the database"]);
+            wp_die();
+        }
     }
     else
     {
